886a1a7256bef1cbf27ac2ac663432ea8fb2548a
angel_in_a_jar <ruslan_tyu@mail.ru>
2022-06-10 13:43:57 +0300

    Initialize `shadow_raytracer` in `ray_tracing_renderer`

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -35,7 +35,10 @@ void cg::renderer::ray_tracing_renderer::init()
 	raytracer->set_index_buffers(model->get_index_buffers());
 
 	lights.push_back({float3{0.f, 1.58f, -0.03f}, float3{0.78f, 0.78f, 0.78f}});
-	// TODO: Lab 2.04. Initialize `shadow_raytracer` in `ray_tracing_renderer`
+
+	shadow_raytracer = std::make_shared<cg::renderer::raytracer<cg::vertex, cg::unsigned_color>>();
+	shadow_raytracer->set_vertex_buffers(model->get_vertex_buffers());
+	shadow_raytracer->set_index_buffers(model->get_index_buffers());
 }
 
 void cg::renderer::ray_tracing_renderer::destroy() {}
@@ -62,12 +65,31 @@ void cg::renderer::ray_tracing_renderer::render()
 		for (auto& light : lights)
 		{
 			cg::renderer::ray to_light(position, light.position - position);
-			result_color += triangle.diffuse * light.color * std::max(dot(normal, to_light.direction), 0.f);
+			auto shadow_payload = shadow_raytracer->trace_ray(
+					to_light, 1, length(light.position - position)
+					);
+			if (shadow_payload.t < 0.f)
+			{
+				result_color += triangle.diffuse * light.color * std::max(dot(normal, to_light.direction), 0.f);
+			}
 		}
 		payload.color = cg::color::from_float3(result_color);
 		return payload;
 	};
+
+	shadow_raytracer->miss_shader = [](const ray& ray)
+	{
+		payload payload{};
+		payload.t = -1.f;
+		return payload;
+	};
+	shadow_raytracer->any_hit_shader = [](const ray& ray, payload& payload, const triangle<cg::vertex>& triangle)
+	{
+		return payload;
+	};
+
 	raytracer->build_acceleration_structure();
+	shadow_raytracer->build_acceleration_structure();
 
 	auto start = std::chrono::high_resolution_clock::now();
 	raytracer->ray_generation(
