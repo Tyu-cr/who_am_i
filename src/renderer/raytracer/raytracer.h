bf7b17e790dd52c76de0c09e05262edd37dcfe06
angel_in_a_jar <ruslan_tyu@mail.ru>
2022-06-10 10:52:39 +0300

    Implement set_vertex_buffers and set_index_buffers of raytracer class

--- src/renderer/raytracer/raytracer.h
+++ src/renderer/raytracer/raytracer.h
@@ -137,12 +137,12 @@ namespace cg::renderer
 	template<typename VB, typename RT>
 	void raytracer<VB, RT>::set_index_buffers(std::vector<std::shared_ptr<cg::resource<unsigned int>>> in_index_buffers)
 	{
-		// TODO: Lab 2.02. Implement set_vertex_buffers and set_index_buffers of raytracer class
+		index_buffers = in_index_buffers;
 	}
 	template<typename VB, typename RT>
 	inline void raytracer<VB, RT>::set_vertex_buffers(std::vector<std::shared_ptr<cg::resource<VB>>> in_vertex_buffers)
 	{
-		// TODO: Lab 2.02. Implement set_vertex_buffers and set_index_buffers of raytracer class
+		vertex_buffers = in_vertex_buffers;
 	}
 
 	template<typename VB, typename RT>
@@ -165,7 +165,6 @@ namespace cg::renderer
 			float3 position, float3 direction,
 			float3 right, float3 up, size_t depth, size_t accumulation_num)
 	{
-		// TODO: Lab 2.01. Implement ray_generation and trace_ray method of raytracer class
 		for (int x = 0; x < width; x++)
 		{
 			for (int y = 0; y < height; y++)
